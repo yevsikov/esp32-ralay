e204bf7eb8377b5cf17568e29e8715b6e78cf138
vladislavyevsikov <vladislav.yevsikov@beetweb.com>
2026-07-14 15:11:32 +0300

    Add volatile for contactStatus

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,10 @@
 
 #define RELAY_SET_MS 1000
 
-
+int counter = 0;
+int summ = 0;
+int average = 0;
+volatile int contactStatus = LOW;
 
 void setup() {
   Serial.begin(115200);
@@ -21,11 +24,14 @@ void setup() {
 }
 
 void loop() {
+  contactStatus = digitalRead(CONTACT_PIN);
+  counter++;
   int startMicros = micros();
   digitalWrite(RELAY_PIN, HIGH);
-  while (digitalRead(CONTACT_PIN) == LOW)
+  while (contactStatus == LOW)
   {
       delay(1);
+      contactStatus = digitalRead(CONTACT_PIN);
   }
   int result = micros() - startMicros ;
 
@@ -34,20 +40,37 @@ void loop() {
   Serial.println(" mcs");
   delay(RELAY_SET_MS);
 
+  summ += result;
+
 
 
   startMicros = micros();
   digitalWrite(RELAY_PIN, LOW);
-    while (digitalRead(CONTACT_PIN) == HIGH)
+    while (contactStatus == HIGH)
   {
       delay(1);
+      contactStatus = digitalRead(CONTACT_PIN);
   }
   result = micros() - startMicros ;
   Serial.print("OFF: ");
   Serial.print(result);
   Serial.println(" mcs");
   delay(RELAY_SET_MS);
+
+  summ += result;
+
+
   Serial.println('_');
+
+  if (counter == 10)
+  {
+    average = summ / counter;
+    Serial.print("Average: ");
+    Serial.print(average);
+    Serial.println(" mcs");
+    counter = 0;
+    summ = 0;
+  }
 
 }
 
