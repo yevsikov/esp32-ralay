6543ad7b1553ddfb03e124a55bcc221e70cea396
vladislavyevsikov <vladislav.yevsikov@beetweb.com>
2026-07-14 14:16:22 +0300

    Add millis to check state

--- src/main.cpp
+++ src/main.cpp
@@ -21,19 +21,26 @@ void setup() {
 }
 
 void loop() {
-
+  int startMillis = millis();
   digitalWrite(RELAY_PIN, HIGH);
+  
   int contactState = digitalRead(CONTACT_PIN);
   Serial.print("Relay state: ");
   Serial.println(contactState);
+  int resultMillis = millis() - startMillis;
+  Serial.println(resultMillis);
   delay(RELAY_SET_MS);
 
-
+  startMillis = millis();
   digitalWrite(RELAY_PIN, LOW);
   contactState = digitalRead(CONTACT_PIN);
   Serial.print("Relay state: ");
   Serial.println(contactState);
+  resultMillis = millis() - startMillis;
+  Serial.println(resultMillis);
   delay(RELAY_SET_MS);
+
+   Serial.println('_');
 
 }
 
